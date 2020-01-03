9c46a6aa40066598ad2a968520f03060cc1d9aad
Sebastián Attoni <sebastianattoni@gmail.com>
2020-01-03 13:18:19 -0300

    repositorio abierto

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "functionscat.h"
+#include "functionstag.h"
+//#include "map.h"
+//#include "list.h"
+
+int main()
+{
+    printf("Hewwo");
+    return 0;
+}
